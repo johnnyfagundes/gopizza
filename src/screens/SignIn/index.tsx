1a467f9f3ed014b6292863ba8b65f90ac4387edf
Johnny Fagundes <johnny.santos@mutua.com.br>
2022-03-17 15:06:29 -0300

    Initial commit

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
+
+import brandImg from '@assets/brand.png';
+
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+import { 
+  Container, 
+  Content, 
+  Title, 
+  Brand, 
+  ForgotPasswordButton, 
+  ForgotPasswordLabel 
+} from "./styles";
+
+export function SignIn() {
+  return (
+    <Container>
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : undefined}>
+        <Content>
+          <Brand source={brandImg} />
+          <Title>Login</Title>
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button title="Entrar" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
+    </Container>
+  );
+}
+
+export default SignIn;
