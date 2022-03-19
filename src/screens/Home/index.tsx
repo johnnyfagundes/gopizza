d432cb43d037b819055be57bd576ddf310f25035
Johnny Fagundes <johnny.santos@mutua.com.br>
2022-03-19 18:07:50 -0300

    Create searchBar inside Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { MaterialIcons } from "@expo/vector-icons";
+import { useTheme } from 'styled-components/native';
 
-import { Container } from './styles';
+import happyEmoji from '@assets/happy.png';
+
+import {
+  Container,
+  Header,
+  Greeting,
+  GreetingEmoji,
+  GreetingText,
+  Title,
+  MenuHeader,
+  MenuItemNumber
+}
+  from './styles';
+import { Search } from '@components/Search';
 
 export default function Home() {
+  const { COLORS } = useTheme();
+
   return (
     <Container>
-        
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin</GreetingText>
+        </Greeting>
+        <TouchableOpacity>
+          <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
+
+      <Search onSearch={() => {}} onClear={() => {}} />
+
+      <MenuHeader>
+        <Title>Cardápio</Title>
+        <MenuItemNumber>10 pizzas</MenuItemNumber>
+      </MenuHeader>
     </Container>
   )
 }
